a247e2955b025f4fd1cdaa88fab413313394a427
root <root@srv1489383.hstgr.cloud>
2026-05-22 09:37:21 +0000

    Fix route build compatibility

--- src/app/dashboard/articles/[id]/page.tsx
+++ src/app/dashboard/articles/[id]/page.tsx
@@ -1,4 +1,4 @@
-﻿'use client'
+'use client';
 
 import { use, useEffect, useMemo, useState } from 'react'
 import Link from 'next/link'
--- src/app/pdf/[slug]/page.tsx
+++ src/app/pdf/[slug]/page.tsx
@@ -1,6 +1,3 @@
-
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
 "use client";
 
 import Link from "next/link";
